ec2781945f0b70fc905415d827df1be172c887e3
Алексей Резниченко <alex@sochi.biz>
2020-10-18 15:37:51 +0300

    Clean

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,5 @@ import { createApp } from 'vue'
 import { router } from './router'
 
 import App from './App.vue'
-// import { useUsers } from './store/useUsers'
-
-// const { userState, authorize } = useUsers()
-// authorize('admin', '123456')
 
 createApp(App).use(router).mount('#app')
