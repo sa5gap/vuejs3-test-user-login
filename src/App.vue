297435bf2cf09be55d26b859b30886a2493c1e9e
Алексей Резниченко <alex@sochi.biz>
2020-10-18 17:03:01 +0300

    Add repo url

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template lang="pug">
   div
     small Vue.js 3.0 (Composition/Options API) + Typescript
+      br
+      a(href="https://github.com/sa5gap/vuejs3-test-user-login") github.com/sa5gap/vuejs3-test-user-login
   img(alt="Vue logo" src="./assets/logo.png")
   router-view
 </template>
