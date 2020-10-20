9de363ed7e95b25d2abb4c4c1b1f4040f300d4fd
Алексей Резниченко <alex@sochi.biz>
2020-10-21 00:47:02 +0300

    Make routable Login view component

---
+++ src/views/Login.vue
@@ -0,0 +1,43 @@
+<template lang="pug">
+  LoginForm(title="Login" @login="onLogin" :error="error")
+</template>
+
+<script lang="ts">
+  import { defineComponent, reactive, toRefs, computed, ref, Ref } from 'vue'
+  import { useRouter } from 'vue-router'
+  import { user, authorize } from '../store/user'
+
+  import LoginForm from '../components/LoginForm.vue'
+
+  export default defineComponent({
+    components: {
+      LoginForm,
+    },
+
+    setup() {
+      const router = useRouter()
+
+      let error = ref<string | null>(null)
+
+      const onLogin = async (name: Ref<string>, password: Ref<string>) => {
+        if (name.value && password.value) {
+          error.value = null
+          try {
+            await authorize(name.value, password.value)
+            if (user.authorized) {
+              router.push({ name: 'contacts' })
+            } else {
+              error.value = user.error
+            }
+          } catch (e) {
+            error.value = e
+          }
+        } else {
+          error.value = 'Please input your name and password!'
+        }
+      }
+
+      return { onLogin, error }
+    },
+  })
+</script>
