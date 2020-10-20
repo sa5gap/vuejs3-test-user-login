6e5410052125e80f1413cb4457e3075cd70b569f
Алексей Резниченко <alex@sochi.biz>
2020-10-21 00:47:29 +0300

    Create reusable login form component

---
+++ src/components/LoginForm.vue
@@ -0,0 +1,49 @@
+<template lang="pug">
+  form
+    h2 {{title}}
+    input(type="text" v-model.trim="name" placeholder="Login")
+    input(type="password" v-model.trim="password" placeholder="Password")
+    small.error(v-if="error") {{ error }}
+    input(type="submit" @click.prevent="login" :value="buttonText")
+</template>
+
+<script lang="ts">
+  import { computed, defineComponent, ref } from 'vue'
+
+  export default defineComponent({
+    props: {
+      title: {
+        type: String,
+        default: 'Login',
+      },
+      error: String,
+      isLoading: Boolean,
+    },
+    setup({ isLoading }, { emit }) {
+      let name = ref('')
+      let password = ref('')
+      let buttonText = computed(() => (isLoading ? 'Please wait...' : 'Login'))
+
+      const login = () => {
+        emit('login', name, password)
+      }
+
+      return { name, password, login, buttonText }
+    },
+  })
+</script>
+
+<style lang="scss" scoped>
+  form {
+    margin: 0 auto;
+
+    > * {
+      display: block;
+      margin: 0 auto 0.5rem auto;
+    }
+
+    .error {
+      color: red;
+    }
+  }
+</style>
